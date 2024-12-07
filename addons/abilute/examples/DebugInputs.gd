7b8f714a4cd040d415d5e5e9caa2b111238314f7
Andreas Nilsson <nilsiker94@gmail.com>
2024-12-07 20:13:48 +0100

    wip

--- addons/abilute/examples/DebugInputs.gd
+++ addons/abilute/examples/DebugInputs.gd
@@ -6,7 +6,7 @@ extends Label
 @export var heal_effect: EffectResource
 @export var stamina_regen_block_effect: EffectResource
 
-
+#region Overrides
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("debug_damage"):
 		target.add_effect(damage_effect)
@@ -16,9 +16,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		target.add_effect(heal_effect)
 	elif event.is_action_pressed("debug_stamina_regen_block"):
 		target.add_effect(stamina_regen_block_effect)
+#endregion
 
-
-
-func _on_abilute_debug_hud_ability_system_changed(ability_system:AbilitySystem) -> void:
-	print("setting new target ", ability_system)
+#region Signal handlers
+func _on_abilute_debug_hud_ability_system_changed(ability_system: AbilitySystem) -> void:
 	target = ability_system
+#endregion
